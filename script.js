c0b25a6deee942c61cec8bf033bad089ebd06ceb
lucasccalmon <lucascarneirocalmon@gmail.com>
2023-08-12 02:08:35 -0300

    Scroll Suave Link interno adicionado

---
+++ script.js
@@ -0,0 +1,18 @@
+function InitScrollSuave() {
+  const linksInternos = document.querySelectorAll('.jsmenu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+InitScrollSuave();
